d1caaaf735d61f6ad7cf842f7d115b95d059e944
Quang KR <dangminhquang.r@gmail.com>
2023-01-11 17:19:05 +0700

    Setup CORS in SignalR server

--- dotnet-sample-signalr/Program.cs
+++ dotnet-sample-signalr/Program.cs
@@ -1,16 +1,31 @@
 using DotnetSampleSignalR.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
+var LocalOrigin = "_allowSpecificOrigins";
 
 builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: LocalOrigin,
+                      policy =>
+                      {
+                          policy
+                                .WithOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod()
+                                .AllowCredentials();
+                      });
+});
 
 var app = builder.Build();
 
 // app.MapGet("/", () => "Hello World!");
 
-app.UseHttpsRedirection();
+// temporary disable HTTPS redirection due to front-end only use HTTP
+// app.UseHttpsRedirection();
 app.UseAuthorization();
+app.UseCors(LocalOrigin);
 
-app.MapHub<ChatHub>("/chatHub");
+app.MapHub<ChatHub>("/chathub");
 
 app.Run();
